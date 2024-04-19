a6b6ace814701fed66ef8771944318f1ce094edb
Tarun kale <130459422+tarunkumarkale@users.noreply.github.com>
2024-04-19 23:49:40 +0530

    Update Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -5,8 +5,7 @@ const Card = ({ id, title, description, author,del}) => {
     <div style={{ backgroundColor: 'black', borderRadius: 'lg', boxShadow: 'xl', padding: '4', display: 'flex',flexDirection:'column', alignItems: 'center',marginTop:'20px',marginLeft:'20px',marginRight:'20px'}}>
       <h1 style={{ textAlign: 'center', textTransform: 'uppercase', fontWeight: 'bold',color:'white' }}>{title}</h1>
       <h1 style={{ textAlign: 'center', textTransform: 'uppercase', fontWeight: 'bold',color:'white'  }}>{author}</h1>
-      <h1 style={{ textAlign: 'center', textTransform: 'uppercase', fontWeight: 'bold',color:'white'  }}>{id}</h1>
-      <p style={{ textAlign: 'center', textTransform: 'uppercase', fontWeight: 'bold',color:'white' }}>{description}</p>
+      <h1 stylee', fontWeight: 'bold',color:'white' }}>{description}</p>
       <button onClick={del}>delete</button>
     </div>
   );
